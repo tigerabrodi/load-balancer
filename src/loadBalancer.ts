f0434366c2313968e7e52b603fabb5706f382838
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-07 17:22:13 +0100

    setup basic load balancer

---
+++ src/loadBalancer.ts
@@ -0,0 +1,38 @@
+import http from 'http'
+
+import httpProxy from 'http-proxy'
+
+const FIRST_SERVER_PORT = 8080
+const SECOND_SERVER_PORT = 8081
+
+// Original list of servers
+const allServers = [
+  `http://localhost:${FIRST_SERVER_PORT}`,
+  `http://localhost:${SECOND_SERVER_PORT}`,
+]
+let currentServer = 0
+
+// Create a proxy server with custom application logic
+const proxy = httpProxy.createProxyServer({})
+
+// Round Robin algorithm
+function getNextServer() {
+  const next = allServers[currentServer]
+  currentServer = (currentServer + 1) % allServers.length
+  return next
+}
+
+// Create your server
+const server = http.createServer((req, res) => {
+  const target = getNextServer()
+
+  if (target) {
+    proxy.web(req, res, { target })
+  } else {
+    res.writeHead(502) // Bad Gateway
+    res.end('No healthy backends available to handle the request.')
+  }
+})
+
+console.log('Load balancer started on port 3000')
+server.listen(3000)
